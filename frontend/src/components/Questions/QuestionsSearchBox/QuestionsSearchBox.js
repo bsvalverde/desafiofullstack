6474dbf370fdc933883649abff6dd32a87a61c1b
Bernardo <bsvalverde@gmail.com>
2019-10-16 15:46:33 -0300

    Estilos da caixa de busca de perguntas e conexão do botão com o back-end (sem passar argumentos)

--- frontend/src/components/Questions/QuestionsSearchBox/QuestionsSearchBox.js
+++ frontend/src/components/Questions/QuestionsSearchBox/QuestionsSearchBox.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 
-const QuestionsSearchBox = () => {
+import * as actions from '../../../store/actions';
+
+import classes from './QuestionsSearchBox.module.scss';
+
+import GreenButton from '../../UI/GreenButton/GreenButton';
+import Input from '../../UI/Input/Input';
+
+const QuestionsSearchBox = (props) => {
   return (
-    <p>SearchBox</p>
+    <div className={classes.QuestionsSearchBox}>
+      <h2>Buscar na API</h2>
+      <div className={classes.Row}>
+        <div className={classes.Input}>
+          <label>Tag</label>
+          <Input />
+        </div>
+        <div className={classes.Input}>
+          <label>Limit</label>
+          <Input />
+        </div>
+        <div className={classes.Input}>
+          <label>Score</label>
+          <Input />
+        </div>
+        <div className={classes.Input}>
+          <label>Sort</label>
+          <Input />
+        </div>
+      </div>
+      <div className={classes.Row}>
+        <div className={classes.Button}>
+          <GreenButton onClick={props.onClickButton}>Buscar</GreenButton>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default QuestionsSearchBox;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClickButton: () => dispatch(actions.searchQuestions())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(QuestionsSearchBox);
