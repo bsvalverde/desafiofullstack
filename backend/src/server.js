a8df14507abfeabd14817553a4d6c9efa4ce4473
Bernardo <bsvalverde@gmail.com>
2019-10-16 00:26:30 -0300

    Adicionado parser json ao servidor backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const resolver = require('./graphql/resolvers');
 const server = express();
 
 server.use(cors());
-// server.use(express.json());
+server.use(express.json());
 server.use('/', graphqlHTTP({
   schema,
   rootValue: resolver
