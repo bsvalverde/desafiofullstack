9b2a708b83984bb4879efd54d4cb01b99f51d2b7
Bernardo <bsvalverde@gmail.com>
2019-10-15 21:34:39 -0300

    Adicionados pacotes e estrutura básica para uso do redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import questionsReducer from './store/reducers/questions';
+
+const rootReducer = combineReducers({
+  questions: questionsReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
