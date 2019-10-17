52267483819aff1bd638e62ce91c491e8ef17b20
Bernardo <bsvalverde@gmail.com>
2019-10-16 22:44:08 -0300

    Adição de componente de Spinner

---
+++ frontend/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './Spinner.module.scss';
+
+export const Spinner = () => {
+  return <p className={classes.Spinner}>Carregando...</p>;
+};
+
+export default Spinner;
