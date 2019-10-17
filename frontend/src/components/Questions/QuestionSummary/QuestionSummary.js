7b81b5d4a78d7014f07d7083fc45257f8f98cfe1
Bernardo <bsvalverde@gmail.com>
2019-10-17 18:00:49 -0300

    QuestionSummary implementado

--- frontend/src/components/Questions/QuestionSummary/QuestionSummary.js
+++ frontend/src/components/Questions/QuestionSummary/QuestionSummary.js
@@ -4,31 +4,57 @@ import classes from './QuestionSummary.module.scss';
 
 const QuestionSummary = (props) => {
   const { question } = props;
+
+  const answerCounterClasses = [classes.Counter];
+  question.answer_count > 0 && (answerCounterClasses.push(classes.HighlightedCounter))
+
+  const date = new Date(question.creation_date * 1000).toLocaleString('pt-br').split(' ')[0];
   return (
     <div className={classes.QuestionSummary}>
       <div className={classes.Counters}>
         <div className={classes.Counter}>
-          <label>Votos: {question.score}</label>
+          <label>{question.score}</label>
+          <br />
+          <label className={classes.CounterIdentifier}>
+            {Math.abs(question.score) * question.score === 1 ? 'ponto' : 'pontos'}
+          </label>
+        </div>
+        <div className={answerCounterClasses.join(' ')}>
+          <label>{question.answer_count}</label>
+          <br />
+          <label className={classes.CounterIdentifier}>
+            {Math.abs(question.answer_count) === 1 ? 'resposta' : 'respostas'}
+          </label>
         </div>
         <div className={classes.Counter}>
-          <label>Respostas: {question.answer_count}</label>
-        </div>
-        <div className={classes.Counter}>
-          <label>Visitas: {question.view_count}</label>
+          <label>{question.view_count}</label>
+          <br />
+          <label className={classes.CounterIdentifier}>
+            {Math.abs(question.view_count) === 1 ? 'visita' : 'visitas'}
+          </label>
         </div>
       </div>
       <div className={classes.Title}>
-        <label>{question.title}</label>
-        <label>{question.link}</label>
+        <a
+          href={question.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {question.title}
+        </a>
       </div>
       <div className={classes.Tags}>
-        <p>{question.tags.map(tag => <label>{tag}</label>)}</p>
+        {question.tags.sort().map(tag => <label key={tag} className={classes.Tag}>{tag}</label>)}
       </div>
       <div className={classes.Info}>
-        <label>{question.creation_date}</label>
-        <label>{new Date(question.creation_date * 1000).toString()}</label>
-        <label>{question.owner.display_name}</label>
-        <label>{question.owner.link}</label>
+        <label>Em <label>{date}</label> por <a
+            href={question.owner.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {question.owner.display_name}
+          </a>
+        </label>
       </div>
     </div>
   );
