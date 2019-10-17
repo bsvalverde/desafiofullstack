e21cc9e508b6fecb3d34cf8e2c1808c1072227cf
Bernardo <bsvalverde@gmail.com>
2019-10-17 16:23:17 -0300

    Ajustada a estrutura do grid do Componente da pergunta

--- frontend/src/components/Questions/QuestionSummary/QuestionSummary.js
+++ frontend/src/components/Questions/QuestionSummary/QuestionSummary.js
@@ -3,7 +3,35 @@ import React from 'react';
 import classes from './QuestionSummary.module.scss';
 
 const QuestionSummary = (props) => {
-	return <p>{props.question.title}</p>;
+  const { question } = props;
+  return (
+    <div className={classes.QuestionSummary}>
+      <div className={classes.Counters}>
+        <div className={classes.Counter}>
+          <label>Votos: {question.score}</label>
+        </div>
+        <div className={classes.Counter}>
+          <label>Respostas: {question.answer_count}</label>
+        </div>
+        <div className={classes.Counter}>
+          <label>Visitas: {question.view_count}</label>
+        </div>
+      </div>
+      <div className={classes.Title}>
+        <label>{question.title}</label>
+        <label>{question.link}</label>
+      </div>
+      <div className={classes.Tags}>
+        <p>{question.tags.map(tag => <label>{tag}</label>)}</p>
+      </div>
+      <div className={classes.Info}>
+        <label>{question.creation_date}</label>
+        <label>{new Date(question.creation_date * 1000).toString()}</label>
+        <label>{question.owner.display_name}</label>
+        <label>{question.owner.link}</label>
+      </div>
+    </div>
+  );
 };
 
 export default QuestionSummary;
