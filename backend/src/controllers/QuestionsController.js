b943ede3b36e3f742b3bc047571c5637c8960939
Bernardo <bsvalverde@gmail.com>
2019-10-18 13:09:38 -0300

    Adicionados valores padrão para os parametros limit, score e sort na API

--- backend/src/controllers/QuestionsController.js
+++ backend/src/controllers/QuestionsController.js
@@ -2,8 +2,7 @@ const stackExchange = require('../services/stackExchange');
 
 class QuestionsController {
   async index(args) {
-    const { tag, score, sort } = args;
-    const limit = args.limit || Number.MAX_SAFE_INTEGER;
+    const { tag, limit, score, sort } = args;
 
     if (!tag) {
       throw new Error('Missing tag');
@@ -12,27 +11,27 @@ class QuestionsController {
     const params = {
       site: 'stackoverflow',
       tagged: tag,
+      limit: limit || 10,
+      score: score || -1,
+      sort: sort || 'activity'
     };
-    sort && (params.sort = sort);
 
     try {
       let questions = [];
       let page = 1;
       let has_more = true;
 
-      while (questions.length < limit && has_more) {
+      while (questions.length < params.limit && has_more) {
         params.page = page;
         const response = await stackExchange.get('/questions', { params });
         let new_questions = response.data.items;
-        if (score) {
-          new_questions = new_questions.filter(question => question.score > score);
-        }
+        new_questions = new_questions.filter(question => question.score > score);
         questions = questions.concat(new_questions);
         has_more = response.data.has_more;
         page = page + 1;
       }
 
-      questions = questions.slice(0, limit);
+      questions = questions.slice(0, params.limit);
 
       return questions;
     } catch (error) {
